75067047d3f2d901aa233a14f6e14694e26aeb57
SeniorWizard <ole.moller@gmail.com>
2022-12-12 20:42:12 +0100

    Day 11 part 1.

---
+++ p11.pl
@@ -0,0 +1,85 @@
+#!/usr/bin/perl
+
+use strict;
+use Data::Dumper;
+
+my $input = 'i11.txt';
+
+open(IN, "$input") || die "cant read $input: $!\n";
+
+my @monkey = ();
+#0: list of items
+#1: operator
+#2: factor
+#3: divisor
+#4: monkeyid for true
+#5: monkeyid for false
+#6: activity
+
+
+my $mid = -1;
+
+while (<IN>) {
+	chomp;
+	if (m/^Monkey\s+(\d+):/) {
+		$mid = $1;
+	} elsif ( m/^\s*$/ ) {
+		#empty line
+		$mid = -1;
+	} elsif (m/Starting items:\s+(.*)\s*$/) {
+		$monkey[$mid]->[0] = [ split(/\D+/, $1) ];
+	} elsif (m/Operation: new = old ([-+\*\/]+)\s+(\d+)/) {
+		$monkey[$mid]->[1] = $1;
+		$monkey[$mid]->[2] = $2;
+	} elsif (m/Operation: new = old \* old/) {
+		$monkey[$mid]->[1] = '**';
+		$monkey[$mid]->[2] = 2;
+	} elsif (m/Test: divisible by (\d+)/) {
+		$monkey[$mid]->[3] = $1;
+	} elsif (m/If true: throw to monkey (\d+)/ ) {
+		$monkey[$mid]->[4] = $1;
+	} elsif (m/If false: throw to monkey (\d+)/ ) {
+		$monkey[$mid]->[5] = $1;
+	} else {
+		print "Not parsable \"$_\"\n";
+	}
+}
+
+my $item;
+my $throwto = -1;
+my $round = 0;
+
+while (++$round <= 20 ) {
+	for ($mid = 0; $mid < scalar(@monkey); $mid++) {
+		#foreach $item (@{$monkey[$mid]->[0]}) {
+		while ($item = shift @{$monkey[$mid]->[0]} ) {
+			#activity
+			$monkey[$mid]->[6]++;
+			#operate
+			$item = eval("$item $monkey[$mid]->[1] $monkey[$mid]->[2]");
+			# unworry
+			$item = int($item / 3);
+			#test
+			if ( $item % $monkey[$mid]->[3] == 0 ) {
+				#true
+				$throwto = $monkey[$mid]->[4];
+			} else {
+				#false
+				$throwto = $monkey[$mid]->[5];
+			}
+			push @{$monkey[$throwto]->[0]}, $item;
+		}
+	}
+}
+
+my @actions = ();
+for ($mid = 0; $mid < scalar(@monkey); $mid++) {
+	push @actions, $monkey[$mid]->[6];
+}
+
+@actions = ( sort { $b <=> $a } @actions);
+
+printf( "Total monkeybusiness: %i\n", $actions[0] * $actions[1]);
+ 
+__END__
+
